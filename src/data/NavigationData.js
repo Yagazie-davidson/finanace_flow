4bc139036c3107e141baeabf3054992e7ba80b08
Letam <davidsonletam@gmail.com>
2023-04-12 23:43:03 +0100

    store data in an external file

---
+++ src/data/NavigationData.js
@@ -0,0 +1,26 @@
+export const NavigationData = [
+	{
+		href: "/",
+		display: "HOME",
+	},
+	{
+		href: "/about",
+		display: "ABOUT",
+	},
+	{
+		href: "/pricing",
+		display: "PRICING",
+	},
+	{
+		href: "/tokens",
+		display: "TOKENS",
+	},
+	{
+		href: "/blog",
+		display: "BLOG",
+	},
+	{
+		href: "/contact",
+		display: "CONTACT US",
+	},
+];
