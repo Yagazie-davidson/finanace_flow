34ec25b7abc993f897588c73115b681e9301e51f
Letam <davidsonletam@gmail.com>
2023-04-16 11:22:01 +0100

    Iphone display

--- src/components/Iphones.jsx
+++ src/components/Iphones.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Iphones() {
 	return (
 		<div className="grid grid-cols-1">
-			<div className="relative">
+			<div className="relative tablet:w-full w-48">
 				<Image
 					src={"/iphone.svg"}
 					className="-rotate-6"
@@ -14,7 +14,7 @@ function Iphones() {
 				/>
 				<Image
 					src={"/iphone.svg"}
-					className="rotate-3 absolute inset-0 left-44"
+					className="rotate-3 absolute inset-0 left-28 tablet:left-28 top-10"
 					alt="iphone"
 					height={10}
 					width={210}
