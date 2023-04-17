412a27e2c4f57d41a919ede9dc828f6c63e76c11
Letam <davidsonletam@gmail.com>
2023-04-17 21:40:58 +0100

    create home content

---
+++ src/components/HomeContent.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Iphones from "./Iphones";
+import Image from "next/image";
+
+function HomeContent() {
+	return (
+		<>
+			<div className="flex justify-between items-center">
+				<Iphones />
+				<div>
+					<h2 className="text-4xl font-bold mb-5">
+						Earn daily rewards on your idle tokens
+					</h2>
+					<p className="text-lg mb-8 max-w-[470px] font-normal">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat
+						nulla suspendisse tortor aene.
+					</p>
+					<div className="flex flex-col space-y-6 text-lg">
+						<div className="flex space-x-10 items-center">
+							<Image alt="icon" src={"/stock.svg"} width={46} height={10} />
+							<p>Lowest fees in market </p>
+						</div>
+						<div className="flex space-x-10 items-center">
+							<Image alt="icon" src={"/lighting.svg"} width={46} height={10} />
+							<p>Fast and secure transactions</p>
+						</div>
+						<div className="flex space-x-10 items-center">
+							<Image alt="icon" src={"/lock.svg"} width={46} height={10} />
+							<p>256-bit secure encryption</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default HomeContent;
