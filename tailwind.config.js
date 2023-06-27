87ff88a1bc081d22c7f5bbf9af574144a9855dad
letam <davidsonletam@gmail.com>
2023-06-27 19:12:41 +0100

    add new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx}",
-		"./src/components/**/*.{js,ts,jsx,tsx}",
-		"./src/app/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				"pri-I": "#0328EE",
-				"pri-II": "#FFFFFF",
-				"pri-III": "#010725",
-				"pri-IV": "#010D50",
-				"pri-V": "#323232",
-			},
-			screens: {
-				tablet: "850px",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "pri-I": "#0328EE",
+        "pri-II": "#FFFFFF",
+        "pri-III": "#010725",
+        "pri-IV": "#010D50",
+        "pri-V": "#323232",
+        "pri-VI": "#1A2561",
+      },
+      screens: {
+        tablet: "850px",
+      },
+    },
+  },
+  plugins: [],
 };
