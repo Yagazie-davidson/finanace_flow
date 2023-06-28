97e355f7f4b2af91888e4956882e3cd432997408
letam <davidsonletam@gmail.com>
2023-06-28 20:58:36 +0100

    modified component to accept icons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,30 @@
+import Image from "next/image";
 import React from "react";
 
-function Button({ text, background, textColor, className }) {
-	return (
-		<button
-			className={`${background || "bg-pri-I"} ${
-				textColor || "text-pri-II"
-			} rounded-3xl py-4 px-8 flex justify-center align-center text-base ${className}`}
-		>
-			{text || "Click me"}
-		</button>
-	);
+function Button({
+  text,
+  background,
+  textColor,
+  className,
+  image,
+  imgAlt,
+  imgWidth,
+  imgHeight,
+  imgSrc,
+}) {
+  return (
+    <button
+      className={`${background || "bg-pri-I"} ${
+        textColor || "text-pri-II"
+      } rounded-3xl py-4 px-8 flex justify-center align-center text-base ${className}`}
+    >
+      {" "}
+      {image && (
+        <Image alt={imgAlt} width={imgWidth} height={imgHeight} src={imgSrc} />
+      )}
+      <div>{text || "Click me"}</div>
+    </button>
+  );
 }
 
 export default Button;
