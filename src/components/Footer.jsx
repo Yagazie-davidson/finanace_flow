3917730a4bfb77457ced4b72d37e3eea104e1018
letam <davidsonletam@gmail.com>
2023-06-28 20:58:56 +0100

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,17 @@ import React from "react";
 import Button from "./Button";
 
 function Footer() {
+  const navigationLinks = [
+    { display: "home", url: "#" },
+    { display: "about", url: "#" },
+    { display: "pricing", url: "#" },
+    { display: "token", url: "#" },
+    { display: "blog", url: "#" },
+    { display: "contact us", url: "#" },
+  ];
   return (
-    <div className="mt-14 bg-pri-IV pt-10 px-12">
-      <div className="flex items-center space-x-3">
+    <div className="mt-14 bg-pri-IV pt-10 px-5">
+      <div className="flex items-center justify-center space-x-3">
         <Image
           alt="finance flow logo"
           src="/header/Frame.png"
@@ -14,17 +22,19 @@ function Footer() {
         />
         <h2 className="text-lg">FinanceFlow</h2>
       </div>
-      <section>
-        <h1>MENU</h1>
+      <section className="mb-10">
+        <h1 className="font-bold text-lg my-4">MENU</h1>
         <hr />
-        <div className="tablet:flex tablet:justify-between items-center">
-          <ul className="gird grid-cols-2">
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>PRICING</li>
-            <li>TOKEN</li>
-            <li>BLOG</li>
-            <li>CONTACT US</li>
+        <div className="tablet:flex tablet:justify-between items-center mt-7">
+          <ul className="grid grid-cols-2 gap-4">
+            {/* Avoid code repetition by using the map method */}
+            {navigationLinks.map((item, index) => {
+              return (
+                <li key={index} className="text-sm font-medium">
+                  <a href={item.url}>{item.display.toUpperCase()}</a>
+                </li>
+              );
+            })}
           </ul>
           <div className="flex flex-col items-center bg-pri-VI mt-10 rounded-3xl p-6">
             <h1 className="text-lg">Download our Application</h1>
@@ -33,12 +43,30 @@ function Footer() {
               sed nulla integer{" "}
             </p>
             <div className="flex flex-col space-y-5 tablet:flex-row tablet:space-y-0 tablet:space-x-5">
-              <Button text={"APP STORE"} />
-              <Button text={"PLAY STORE"} />
+              <Button
+                text={"APP STORE"}
+                image
+                imgAlt={"app-store-icon"}
+                imgHeight={10}
+                imgWidth={25}
+                imgSrc={"/appstore.svg"}
+                className={"space-x-3 w-64"}
+              />
+              <Button
+                text={"PLAY STORE"}
+                image
+                imgAlt={"app-store-icon"}
+                imgHeight={10}
+                imgWidth={25}
+                imgSrc={"/playstore.svg"}
+                className={"space-x-3"}
+              />
             </div>
           </div>
         </div>
       </section>
+      <hr />
+      <p className="text-center py-10">All rights reserved</p>
     </div>
   );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,37 @@
 import React from "react";
 import { DM_Sans } from "next/font/google";
 import Image from "next/image";
+import Footer from "./Footer";
 
 const dmSans = DM_Sans({
-	subsets: ["latin"],
-	weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 function Layout({ children, className }) {
-	return (
-		<div
-			className={`${dmSans.className} bg-pri-III text-pri-II h-fit ${className}`}
-		>
-			<Image
-				alt="vector"
-				src="/vector-I.svg"
-				height={10}
-				width={500}
-				className="row-start-1 col-start-1"
-			/>
-			<Image
-				alt="vector"
-				src="/shadow.svg"
-				height={10}
-				width={500}
-				className="absolute top-80 tablet:top-40 right-0"
-			/>
+  return (
+    <div
+      className={`${dmSans.className} bg-pri-III text-pri-II h-fit ${className}`}
+    >
+      <Image
+        alt="vector"
+        src="/vector-I.svg"
+        height={10}
+        width={500}
+        className="row-start-1 col-start-1"
+      />
+      <Image
+        alt="vector"
+        src="/shadow.svg"
+        height={10}
+        width={500}
+        className="absolute top-80 tablet:top-40 right-0"
+      />
 
-			{children}
-		</div>
-	);
+      {children}
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
